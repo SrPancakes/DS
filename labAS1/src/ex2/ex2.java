e03dede06cc9afd1481904ead0862aba31b57027
Rodrigo Meijome Quintana <rodrigo.meijome@udc.es>
2023-09-20 18:17:10 +0200

    Ejercicio 2 - Constructores

---
+++ labAS1/src/ex2/ex2.java
@@ -0,0 +1,17 @@
+package ex2;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        /*InmutableMatrix m = new InmutableMatrix(4, 4);
+        for(int[] i : m.getArray()){
+            for(int j : i){
+                System.out.println(j);
+            }
+        }*/
+
+        int[][] arr = {{1,2,3}, {2,3}};
+        InmutableMatrix m = new InmutableMatrix(arr);
+    }
+
+}
