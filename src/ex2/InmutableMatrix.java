f3a3f8e25ea28e1a667f944255dc1b4a07887fb7
Rodrigo <rodrigo.meijome@udc.es>
2023-09-25 18:10:52 +0200

    toString method

---
+++ src/ex2/InmutableMatrix.java
@@ -0,0 +1,47 @@
+package ex2;
+
+public class InmutableMatrix {
+
+    private int[][] arr;
+
+    public InmutableMatrix(int[][] arr){
+        int aux = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(aux == 0) aux = arr[i].length;
+            else if(aux != arr[i].length) throw new IllegalArgumentException();
+        }
+        this.arr = arr;
+    }
+
+    public InmutableMatrix(int filas, int columnas){
+        this.arr = new int[filas][columnas];
+        if(filas <= 0 || columnas <= 0) throw new IllegalArgumentException();
+        int aux = 0;
+        for(int i = 0; i < filas; i++){
+            for(int j = 0; j < columnas; j++){
+                this.arr[i][j] = aux;
+                aux++;
+            }
+        }
+    }
+
+    public String toString(){
+        String res = "";
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                res += this.arr[i][j] + " ";
+            }
+            res += "\n";
+        }
+        return res;
+    }
+
+    public int[][] getArray(){
+        return this.arr;
+    }
+
+    public void setArray(int[][] arr){
+        this.arr = arr;
+    }
+
+}
