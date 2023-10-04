78b1d651b6c0ef95d1a5b7630ed14ca479735857
Rodrigo <rodrigo.meijome@udc.es>
2023-10-04 17:03:15 +0200

    Project update

---
+++ Practicas/P0/src/ex2/ex2.java
@@ -0,0 +1,10 @@
+package ex2;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        InmutableMatrix m = new InmutableMatrix(4,4);
+        System.out.println(m);
+    }
+
+}
